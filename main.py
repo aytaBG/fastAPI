5291b4c7276416533dd94a887ab8fba7a104e83a
aytaBG <aytaB12@gmail.com>
2024-04-15 14:23:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+from typing import List, Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from enum import Enum
+
+app = FastAPI(
+    title="Trading App"
+)
+
+#DATABASES
+
+'''
+fake_users = [
+    {'id': 1, 'role': 'admin', 'name': 'Bob'},
+    {'id': 2, 'role': 'investor', 'name': 'Kek'},
+    {'id': 3, 'role': 'trader', 'name': 'Kok'},
+    {'id': 4, 'role': 'trader', 'name': 'Kok', 'degree': [
+        {'id': 1, 'created_at': '2024-04-07T15:01:28', 'type_degree': 'newbie'}
+    ]},
+]
+
+fake_trades = [
+    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'ammount': 2.12},
+    {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'ammount': 2.12}
+]
+'''
+
+
+
+#CLASSES
+
+class DegreeType(Enum):
+    newbie = 'newbie'
+    expert = 'expert'
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = None
+
+#ENDPOINTS
+
+#Возврат информации про юзера по его id
+@app.get("/users/{user_id}", response_model=List[User])
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get('id') == user_id]
+
+#Возврат информации о сделках по их id
+@app.get('/trades')
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+#Смена юзернейма по id
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
+    current_user['name'] = new_name
+    return {'status': 200, 'data': current_user}
+
+#Добавить сделку
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {'status': 200, 'data': fake_trades}
+
