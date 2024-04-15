2675cc1e4d94eb74c6ea6d27e54ddeb74b73fe3a
aytaBG <aytaB12@gmail.com>
2024-04-15 15:41:35 +0300

    Прогресс после 4 урока

---
+++ models/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON
+import datetime
+
+metadata = MetaData()
+
+roles = Table(
+    'roles',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String, nullable=False),
+    Column('permissions', JSON)
+)
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('email', String, nullable=False),
+    Column('username', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('registered_at', TIMESTAMP, default=datetime.UTC),
+    Column('role_id', Integer, ForeignKey('roles.id')),
+)
+
