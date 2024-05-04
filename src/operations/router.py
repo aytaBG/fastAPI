623b9d6f915697ccc040cae9bafa4367334be53b
aytaBG <aytaB12@gmail.com>
2024-05-04 23:59:06 +0300

    Прогресс после 7 урока

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
+
 
 router = APIRouter(
     prefix='/operations',
@@ -15,15 +16,37 @@ router = APIRouter(
 
 @router.get('/')
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    print(query)
-    result = await session.execute(query)
-    return result.mappings().all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        print(query)
+        result = await session.execute(query)
+        return {
+            'status': 'success',
+            'data': result.mappings().all(),
+            'details': None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            'status': 'error',
+            'data': None,
+            'details': None
+        })
 
 
 @router.post('/')
 async def add_spespecific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(operation).values(**new_operation.dict())
-    await session.execute(stmt)
-    await session.commit()
-    return {'status': 'success'}
+    try:
+        stmt = insert(operation).values(**new_operation.dict())
+        await session.execute(stmt)
+        await session.commit()
+        return {
+            'status': 'success',
+            'data': None,
+            'details': None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            'status': 'error',
+            'data': None,
+            'details': None
+        })
